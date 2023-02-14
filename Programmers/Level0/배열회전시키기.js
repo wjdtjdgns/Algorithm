31552051c33d02cc26d58645fa7e5a4ab5aa7857
wjdtjdgns <person3275@gmail.com>
2023-02-14 22:54:08 +0900

    add: 출처 추가

--- Programmers/Level0/배열회전시키기.js
+++ Programmers/Level0/배열회전시키기.js
@@ -4,3 +4,5 @@ function solution(numbers, direction) {
     : numbers.push(numbers.shift());
   return numbers;
 }
+
+// 출처 : 프로그래머스 코딩테스트 연습, https://school.programmers.co.kr/learn/courses/30/lessons/120844
--- Programmers/Level0/최댓값만들기(2).js
+++ Programmers/Level0/최댓값만들기(2).js
@@ -5,3 +5,5 @@ function solution(numbers) {
     numbers[numbers.length - 2] * numbers[numbers.length - 1]
   );
 }
+
+// 출처 : 프로그래머스 코딩테스트 연습, https://school.programmers.co.kr/learn/courses/30/lessons/120862
