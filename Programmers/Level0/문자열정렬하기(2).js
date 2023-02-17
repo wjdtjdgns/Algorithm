2ccbe1ec00cf1e1a3fd611df312ab4d73d1f7c23
wjdtjdgns <person3275@gmail.com>
2023-02-17 21:29:18 +0900

    solve: 문자열 정렬하기 (2)

---
+++ Programmers/Level0/문자열정렬하기(2).js
@@ -0,0 +1,5 @@
+function solution(my_string) {
+  return [...my_string.toLowerCase()].sort().join("");
+}
+
+// 출처 : 프로그래머스 코딩테스트 연습, https://school.programmers.co.kr/learn/courses/30/lessons/120911
