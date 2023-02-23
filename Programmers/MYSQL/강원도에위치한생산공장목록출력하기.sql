1252f65667824bed11b19d7d6734a526905fa0a7
wjdtjdgns <person3275@gmail.com>
2023-02-23 22:10:39 +0900

    주석 추가

--- Programmers/MYSQL/강원도에위치한생산공장목록출력하기.sql
+++ Programmers/MYSQL/강원도에위치한생산공장목록출력하기.sql
@@ -2,3 +2,5 @@ SELECT FACTORY_ID, FACTORY_NAME, ADDRESS
 FROM FOOD_FACTORY
 WHERE ADDRESS LIKE '강원도%'
 ORDER BY FACTORY_ID
+
+-- 출처 : 프로그래머스 코딩테스트 연습 : https://school.programmers.co.kr/learn/courses/30/lessons/131112
