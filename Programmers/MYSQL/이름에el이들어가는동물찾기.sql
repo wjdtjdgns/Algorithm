eb1e02d9e58ad7490e228e35360dbca9130052b3
wjdtjdgns <person3275@gmail.com>
2023-02-24 21:30:33 +0900

    solve: 이름에 el이 들어가는 동물 찾기

---
+++ Programmers/MYSQL/이름에el이들어가는동물찾기.sql
@@ -0,0 +1,6 @@
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE ANIMAL_TYPE = "Dog" AND NAME LIKE "%EL%"
+ORDER BY NAME ASC
+
+-- 출처 : 프로그래머스 코딩테스트 연습, https://school.programmers.co.kr/learn/courses/30/lessons/59047
