6055f1b04c45cd3c8caefaab08e5753b866a3cd0
wjdtjdgns <person3275@gmail.com>
2023-05-12 13:27:57 +0900

    solve: 14425

---
+++ Baekjoon/14425.js
@@ -0,0 +1,22 @@
+const [num, ...input] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [N, M] = num.split(" ").map(Number);
+const map = new Map();
+let count = 0;
+
+for (let i = 0; i < N; i++) {
+  map.set(input[i], true);
+}
+
+for (let i = N; i < N + M; i++) {
+  if (map.has(input[i])) {
+    count++;
+  }
+}
+
+console.log(count);
+// 출처 : Baekjoon online judge, https://www.acmicpc.net/problem/14425
