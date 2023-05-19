74e8e4ec24e2ba8a1072194574be7a4920c34d19
wjdtjdgns <person3275@gmail.com>
2023-05-19 12:31:51 +0900

    solve: 1269

---
+++ Baekjoon/1269.js
@@ -0,0 +1,22 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const A = new Set(...[input[1].split(" ")]);
+const B = new Set(...[input[2].split(" ")]);
+
+const Amap = [];
+const Bmap = [];
+
+A.forEach(v => {
+  if(!B.has(v)) {
+    Amap.push(v);
+  }
+})
+
+B.forEach(v => {
+  if(!A.has(v)) {
+    Bmap.push(v);
+  }
+})
+console.log(Amap.length + Bmap.length);
+
+// 출처 : Baekjoon online judge, https://www.acmicpc.net/problem/1269
