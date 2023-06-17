c721b416f6dc45a1f39fe3f2b1a724f6ea232c38
wjdtjdgns <person3275@gmail.com>
2023-06-17 20:45:45 +0900

    solve: 1934

---
+++ Baekjoon/1934.js
@@ -0,0 +1,22 @@
+const gcd = (a, b) => {
+  if (b === 0) return a;
+  return gcd(b, a % b);
+};
+
+const [N, ...input] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const result = [];
+
+for (const v of input) {
+  const [a, b] = v.split(" ").map((i) => parseInt(i));
+  const lcm = (a * b) / gcd(a, b);
+  result.push(lcm);
+}
+
+console.log(result.join("\n"));
+
+// 출처 : Baekjoon online judge, https://www.acmicpc.net/problem/1934
