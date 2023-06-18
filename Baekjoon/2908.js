b9e45f6b1c3c899fc218255a40cba103f28097ef
wjdtjdgns <person3275@gmail.com>
2023-06-18 23:41:49 +0900

    solve: 2908

---
+++ Baekjoon/2908.js
@@ -0,0 +1,14 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ");
+
+const arr = input
+  .map((v) => v.split("").reverse().join(""))
+  .map((v) => parseInt(v))
+  .sort((a, b) => a - b);
+
+console.log(arr[1]);
+
+// https://www.acmicpc.net/problem/2908
