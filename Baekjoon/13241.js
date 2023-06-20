e298c4bf51b31237d3426c3b78e9132daebba6cd
wjdtjdgns <person3275@gmail.com>
2023-06-20 22:17:55 +0900

    solve: 13241

---
+++ Baekjoon/13241.js
@@ -0,0 +1,27 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let lines = [];
+
+rl.on("line", function (line) {
+  lines.push(line);
+}).on("close", function () {
+  const [A, B] = lines[0].split(" ").map((v) => BigInt(v));
+  const gcd = (a, b) => {
+    while (b !== 0n) {
+      let temp = b;
+      b = a % b;
+      a = temp;
+    }
+    return a;
+  };
+
+  let divisor = gcd(A, B);
+  console.log(((A / divisor) * B).toString());
+});
+
+// 출처 : Baekjoon online judge, https://www.acmicpc.net/problem/13241
