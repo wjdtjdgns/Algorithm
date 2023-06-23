61d21cdd982befa16782420a2c97539ff97ecdaf
wjdtjdgns <person3275@gmail.com>
2023-06-23 09:26:08 +0900

    solve: 1546

---
+++ Baekjoon/1546.js
@@ -0,0 +1,13 @@
+const [N, input] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const arr = input.split(" ").map(v => +v).sort((a, b) => a - b);
+
+const MAX = arr[+N - 1];
+
+const avg = arr.reduce((acc, cur) => {
+  return acc += (cur / MAX) * 100;
+}, 0) / +N
+
+console.log(avg);
+
+// 출처 : Baekjoon online judge, https://www.acmicpc.net/problem/1546
