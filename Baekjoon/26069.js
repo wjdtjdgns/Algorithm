ea1c04fa52a033ba19e292c984bd28cba37c4cda
wjdtjdgns <person3275@gmail.com>
2023-06-25 18:24:33 +0900

    solve: 26069

---
+++ Baekjoon/26069.js
@@ -0,0 +1,15 @@
+const [N,...input] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const arr = input.map(v => v.split(' '));
+const dance = new Set(["ChongChong"]);
+
+for(let i = 0; i<+N; i++) {
+  if(dance.has(arr[i][0]) || dance.has(arr[i][1])) {
+    dance.add(arr[i][0]);
+    dance.add(arr[i][1]);
+  }
+}
+
+console.log(dance.size);
+
+// 출처 : Baekjoon online judge, https://www.acmicpc.net/problem/26069
